3df1ba93438ea9cd9c1a257fd6bec89e5771ed1a
qhddusl <qhddusl2004@naver.com>
2021-08-04 01:34:22 +0900

    Commit Sample Test Setting

---
+++ src/test/java/com/vermouth/service/SampleServiceTest.java
@@ -0,0 +1,39 @@
+package com.vermouth.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleServiceTest {
+
+    @Test
+    @DisplayName("Sample 생성")
+    @Deprecated
+    void create() {
+    }
+
+    @Test
+    @DisplayName("Sample 리스트 읽기")
+    @Deprecated
+    void readList() {
+    }
+
+    @Test
+    @DisplayName("Sample 한개 읽기")
+    @Deprecated
+    void readOne() {
+    }
+
+    @Test
+    @DisplayName("Sample 수정")
+    @Deprecated
+    void update() {
+    }
+
+    @Test
+    @DisplayName("Sample 삭제")
+    @Deprecated
+    void delete() {
+    }
+}
